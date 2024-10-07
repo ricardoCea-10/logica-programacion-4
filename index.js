4314433d79fffa618874001263afbfccafbe1504
ricardoCea <ricardo.cea@genstudents.org>
2024-10-06 21:58:10 -0300

    Ricardo Cea - Tarea actualizada: Ejercicio de lógica de programación 4

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ Debe solicitar al usuario un número por prompt o por input y guardarlo.
     - Debe imprimir el resultado por consola o por el DOM.
     - Debe ser capaz de identificar si el dato de entrada es de tipo number, en caso contrario debe mandar un mensaje de error y volver a solicitar el dato.
     - Prueba tu programa con las siguientes combinaciones para asegurarte que funcione correctamente:
+Entrada: 3
+Salida: 0, 1, 1.
+
+Entrada: 10
+Salida: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
+
+Pruébalo con los números que se te ocurran.
 */
 
 // Bienvenida al usuario
